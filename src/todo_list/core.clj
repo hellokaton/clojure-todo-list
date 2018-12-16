c3d797ec3ed6797c1164cd13d303cba5df76133c
biezhi <biezhi.me@gmail.com>
2018-12-17 00:39:26 +0800

    :sparkles: add restful route

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -7,24 +7,28 @@
 
 (defn welcome
   "这是欢迎页"
-  [request]
-  {:status 200
-   :body "<h1>你好，这是欢迎页面！</h1>"
+  [_]
+  {:status  200
+   :body    "<h1>你好，这是欢迎页面！</h1>"
    :headers {"Content-Type" "text/html;charset=UTF-8"}})
 
 (defn about
   "这是关于页面"
-  [request]
-  {:status 200
-   :body "我是一个写代码的，只有我享受这无聊的事！"
+  [_]
+  {:status  200
+   :body    "我是一个写代码的，只有我享受这无聊的事！"
    :headers {}})
 
 (defn hello
   "一个 restful 风格的路由"
   [request]
+
+  ; 这样写让代码更简洁
+  ; 和 (let [name (:name (:route-params request))]) 等效
+  ; 和 (let [name (get (get request :route-params) :name)]) 等效
   (let [name (get-in request [:route-params :name])]
-    {:status 200
-     :body (str "你好 " name "，你终于访问这个 URL 了！")
+    {:status  200
+     :body    (str "<h1>你好 " name "，你终于访问这个 URL 了！</h1>")
      :headers {"Content-Type" "text/html;charset=UTF-8"}}))
 
 (defroutes app
@@ -32,14 +36,14 @@
   (GET "/about" [] about)
   (GET "/hello/:name" [] hello)
   (GET "/request-info" [] handle-dump)
-  (not-found "<h1>没有找到你要请求的资源</h1>
-              <p>这是一篇无人知晓的荒地！</p>"))
+  (not-found "<h1>有找到你要请求的资源</h1>
+    <p>这是一篇无人知晓的荒地！</p>"))
 
 (defn -main
   "一个 Ring 的 Hello World 程序"
   [port]
   (jetty/run-jetty app
-    {:port (Integer. port)}))
+                   {:port (Integer. port)}))
 
 (defn -dev-main
   "开发环境热加载可还行"
